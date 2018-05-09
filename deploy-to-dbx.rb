7bb4455157eee26d4cf2f0153fcc1ed12c1dcf74
Janos Erdos <janos.erdos@dbx.hu>
2018-05-09 15:03:29 +0200

    deploy script should do full

--- deploy-to-dbx.rb
+++ deploy-to-dbx.rb
@@ -1,4 +1,10 @@
 #!/bin/env ruby
+
+puts (`lein clean` or abort("Could not clean up. No leiningen?"))
+puts (`lein pom` or abort("Could not make pom file"))
+puts (`lein compile` or abort("Could not compile!"))
+puts (`mvn package -DskipTests` or abort("Could not make jar package"))
+
 JARFILE = Dir.glob("target/stencil-*.jar").find { |fn| not fn.include? "-standalone" }
 
 abort('Missing jar file, build it first!') unless JARFILE
@@ -12,9 +18,9 @@ puts "Artifakt: #{ARTIFACT}"
 puts "Verzio: #{VERSION}"
 
 REMOTE_DIRECTORY="/home/ubuntu/maven/repository/#{GROUP}/#{ARTIFACT}/#{VERSION}"
-puts `ssh dbx.services mkdir -p #{REMOTE_DIRECTORY}`
+puts (`ssh dbx.services mkdir -p #{REMOTE_DIRECTORY}` or abort("Could not create dir"))
 
 REMOTE_FILE="#{REMOTE_DIRECTORY}/stencil-#{VERSION}.jar"
-puts `scp #{JARFILE} dbx.services:#{REMOTE_FILE}`
+puts (`scp #{JARFILE} dbx.services:#{REMOTE_FILE}` or abort("Could not copy!"))
 
 puts "Good!"
