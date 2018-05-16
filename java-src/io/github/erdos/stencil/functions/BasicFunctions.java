cd68ded47721fa8e3ba902534bb0b536a2a74d9a
Janos Erdos <janos.erdos@dbx.hu>
2018-05-16 21:47:12 +0200

    add empty function

--- java-src/io/github/erdos/stencil/functions/BasicFunctions.java
+++ java-src/io/github/erdos/stencil/functions/BasicFunctions.java
@@ -21,6 +21,23 @@ public enum BasicFunctions implements Function {
                     return arg;
             return null;
         }
+    },
+
+    /**
+     * Returns true iff input is null, empty string or empty collection.
+     * <p>
+     * Expects exaclty 1 argument.
+     */
+    EMPTY() {
+        @Override
+        public Object call(Object... arguments) throws IllegalArgumentException {
+            if (arguments.length != 1)
+                throw new IllegalArgumentException("empty() function expects exactly 1 argument, " + arguments.length + " given.");
+            Object x = arguments[0];
+            return (x == null || "".equals(x))
+                    || ((x instanceof Collection) && ((Collection) x).size() == 0)
+                    || ((x instanceof Iterable) && !((Iterable) x).iterator().hasNext());
+        }
     };
 
     @Override
