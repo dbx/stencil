45af3d64a541e87735f1b609fc6e8f5f08e02d9c
Janos Erdos <janos.erdos@dbx.hu>
2018-05-23 10:11:15 +0200

    basic functions fixes + test cases

--- java-src/io/github/erdos/stencil/functions/BasicFunctions.java
+++ java-src/io/github/erdos/stencil/functions/BasicFunctions.java
@@ -9,6 +9,34 @@ import java.util.Collection;
 public enum BasicFunctions implements Function {
 
     /**
+     * Selects value based on first argument.
+     * Usage: switch(expression, case-1, value-1, case-2, value-2, ..., default-value)
+     */
+    SWITCH() {
+        @Override
+        public Object call(Object... arguments) throws IllegalArgumentException {
+            if (arguments.length < 3) {
+                throw new IllegalArgumentException("switch() function expects at least 3 args!");
+            }
+            final Object expr = arguments[0];
+            for (int i = 1; i < arguments.length; i += 2) {
+                final Object value = arguments[i];
+                final Object result = arguments[i + 1];
+                if (expr == null && value == null)
+                    return result;
+                else if (expr != null && expr.equals(value))
+                    return result;
+            }
+
+            if (arguments.length % 2 == 0) {
+                return arguments[arguments.length - 1];
+            } else {
+                return null;
+            }
+        }
+    },
+
+    /**
      * Returns the first non-null an non-empty value.
      * <p>
      * Accepts any arguments. Skips null values, empty strings and empty collections.
@@ -17,7 +45,7 @@ public enum BasicFunctions implements Function {
         @Override
         public Object call(Object... arguments) {
             for (Object arg : arguments)
-                if (arg != null && !"".equals(arg) && (!(arg instanceof Collection) || ((Collection) arg).isEmpty()))
+                if (arg != null && !"".equals(arg) && (!(arg instanceof Collection) || !((Collection) arg).isEmpty()))
                     return arg;
             return null;
         }
