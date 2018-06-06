a246a2390747a55c2b47b9e0132226aaf9c146cb
Janos Erdos <janos.erdos@dbx.hu>
2018-06-06 19:56:00 +0200

    set version to 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.erdos/stencil "0.1.6-SNAPSHOT"
+(defproject io.github.erdos/stencil "0.1.6"
   :description       "Templating engine for office documents."
   :min-lein-version  "2.0.0"
   :java-source-paths ["java-src"]
@@ -9,8 +9,7 @@
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.2.0-alpha5"]
-                 [org.jodconverter/jodconverter-local "4.1.0"] ;; pdf conversion
-                 ]
+                 [org.jodconverter/jodconverter-local "4.1.0"]] ;; pdf conversion
   :pom-addition ([:properties ["maven.compiler.source" "8"] ["maven.compiler.target" "8"]])
   :plugins [[quickie "0.4.2" :exclusions [org.clojure/clojure]]
             [lein-javadoc "0.3.0"]
@@ -18,9 +17,7 @@
   :aliases      {"junit" ["with-profile" "test" "do" "test-out" "junit" "junit.xml"]}
   :javadoc-opts {:package-names ["io.github.erdos.stencil"]
                  :additional-args ["-overview" "java-src/overview.html"
-                                   "-top" "<style>kbd{background:#ddd}; a[title~=class], a[title~=interface], a[title~=enum]{text-decoration: underline; font-weight: bold} dd>code{background:#eee}</style>"
-                                   ]}
-
+                                   "-top" "<style>kbd{background:#ddd}; a[title~=class], a[title~=interface], a[title~=enum]{text-decoration: underline; font-weight: bold} dd>code{background:#eee}</style>"]}
   :main io.github.erdos.stencil.standalone.Main
   :jar-exclusions [#".*\.xml"]
   :profiles {:test {:dependencies [[junit/junit "4.12"]
