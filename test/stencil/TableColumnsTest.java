4e526ec6fe1dfac1e2c3e0cb8ea8e159dcbeb234
Janos Erdos <janos.erdos@dbx.hu>
2018-09-30 09:29:52 +0200

    added table test case

--- test/stencil/TableColumnsTest.java
+++ test/stencil/TableColumnsTest.java
@@ -20,12 +20,39 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.joining;
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 public class TableColumnsTest {
+
+    /**
+     * All table columns are visible.
+     */
+    @Test
+    public void testAllColumnsVisible() {
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("testerName", "Wizard");
+        data.put("col1", true);
+        data.put("col2", true);
+        data.put("col3", true);
+        data.put("col4", true);
+
+        testWithData("test-table-columns.docx",
+                data,
+                asList("Hello Wizard!",
+                        "A1", "A2", "A3", "A4",
+                        "B1+B2", "B3", "B4",
+                        "C1", "C2+C3", "C4",
+                        "D1", "D2", "D3+D4",
+                        "E1+E2+E3", "E4",
+                        "F1", "F2+F3+F4",
+                        "G1+G2+G3+G4"),
+                emptyList());
+    }
 
     /**
      * Hiding first column must not affect other columns.
