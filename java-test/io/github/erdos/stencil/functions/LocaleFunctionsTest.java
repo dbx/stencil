60ede9aba4d151db6cfb1e67abc400a833e9b000
Janos Erdos <janos.erdos@dbx.hu>
2018-10-01 11:44:24 +0200

    asd

--- java-test/io/github/erdos/stencil/functions/LocaleFunctionsTest.java
+++ java-test/io/github/erdos/stencil/functions/LocaleFunctionsTest.java
@@ -13,6 +13,9 @@ public class LocaleFunctionsTest {
     public void testCurrency() {
         assertNull(CURRENCY.call(new Object[]{null}));
         assertEquals("123 Ft", CURRENCY.call(123, "HU-HU"));
+        assertEquals("123,45 Ft", CURRENCY.call(123.45, "HU-HU"));
+        assertEquals("0 Ft", CURRENCY.call(0.0, "HU-HU"));
+        assertEquals("-1 Ft", CURRENCY.call(-1, "HU-HU"));
     }
 
     @Test
@@ -20,5 +23,7 @@ public class LocaleFunctionsTest {
         assertNull(PERCENT.call(new Object[]{null}));
         assertEquals("12 300%", PERCENT.call(123, "HU-HU"));
         assertEquals("95%", PERCENT.call(0.95, "HU-HU"));
+        assertEquals("1%", PERCENT.call(0.0095, "HU-HU"));
+        assertEquals("-200%", PERCENT.call(-2, "HU-HU"));
     }
 }
